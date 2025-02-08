b1e40aa8894099fd5a62fd03eb6af1ef9b2a6346
Ananya Banka <139055117+ananyabanka@users.noreply.github.com>
2025-02-08 17:02:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// create variables to store the path to your image files
+const img1 = "./img/lego-1.jpg";
+
+
+const img2 = "./img/lego-2.jpg";
+
+const img3 = "./img/lego-3.jpg";
+
+const img5 = "./img/lego-5.png";
+
+// define your images here
+const images = [ img1,img2, img3, img5];
+
+// // this chooses a random number from all available image indices
+// var randomImage = images[Math.floor(Math.random() * images.length)];
+// // make the URL into a proper image tag
+// var image = "<img src='" + randomImage + "'>";
+// // append to the div
+// document.getElementById("valentines-container").innerHTML = image;
+
+// const refreshButton = document.getElementById('refreshButton');
+
+// refreshButton.addEventListener('click', function() {
+//   location.reload();
+// });
+
+// Function to load a random image
+function loadRandomImage() {
+    var randomImage = images[Math.floor(Math.random() * images.length)];
+    var image = `<img src="${randomImage}" alt="Valentine's LEGO">`;
+    document.getElementById("valentines-container").innerHTML = image;
+}
+
+// Run the function when the page loads
+window.onload = loadRandomImage;
+
+// Add event listener to refresh button
+document.getElementById("refreshButton").addEventListener("click", loadRandomImage);
